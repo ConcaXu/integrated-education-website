1d899dff29324f458b1b7e7a4e02164ed3fbb954
zhangxu <2424858916@qq.com>
2026-04-19 11:26:10 +0800

    feat:add package

--- vite.config.ts
+++ vite.config.ts
@@ -44,7 +44,7 @@ export default defineConfig(({ mode }) => {
       host: "0.0.0.0",
       proxy: {
         "/api": {
-          target: "http://XXXXXXXXXXXXXXXXXXXXXXXXXXX",
+          target: "http://127.0.0.1:8080",
           changeOrigin: true,
           rewrite: (path) => path.replace(/^\/api/, ""),
         },

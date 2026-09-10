80f18221c2f63107bd04690402537e47a94f7b43
Mabaike <2945145074@qq.com>
2026-09-10 15:24:30 +0800

    已完成导航激活态同步。

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -5,23 +5,23 @@
         ><img src="/images/logo.webp" alt="APIMTC" /></router-link
       ><button class="menu-toggle" @click="open = !open">☰</button>
       <nav :class="{ open }">
-        <router-link to="/">{{ tx("Home", "首页") }}</router-link
-        ><router-link to="/about">{{
+        <router-link to="/" exact-active-class="is-active">{{ tx("Home", "首页") }}</router-link
+        ><router-link to="/about" exact-active-class="is-active">{{
           tx("About APIMTC", "关于 APIMTC")
         }}</router-link
-        ><router-link to="/mobility">{{
+        ><router-link to="/mobility" exact-active-class="is-active">{{
           tx("Education Mobility", "教育流动")
         }}</router-link
-        ><router-link to="/mice-business">{{
+        ><router-link to="/mice-business" exact-active-class="is-active">{{
           tx("MICE & Business", "MICE 与商务")
         }}</router-link
-        ><router-link to="/partnerships">{{
+        ><router-link to="/partnerships" exact-active-class="is-active">{{
           tx("Partnerships", "国际合作")
         }}</router-link
-        ><router-link to="/china-gateway">{{
+        ><router-link to="/china-gateway" exact-active-class="is-active">{{
           tx("China Gateway", "中国门户")
         }}</router-link
-        ><router-link class="header-cta" to="/contact">{{
+        ><router-link class="header-cta" to="/contact" exact-active-class="is-active">{{
           tx("Contact Us", "联系我们")
         }}</router-link
         ><button class="lang-button" @click="toggleLang">
